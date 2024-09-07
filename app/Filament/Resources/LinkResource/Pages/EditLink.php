37a14bd6b461c46cf74263a52ec45956a2c434ca
Ishaan Sheikh <sheikh.ysbh@gmail.com>
2024-09-07 19:41:40 +0530

    added visit link action

--- app/Filament/Resources/LinkResource/Pages/EditLink.php
+++ app/Filament/Resources/LinkResource/Pages/EditLink.php
@@ -4,7 +4,9 @@ namespace App\Filament\Resources\LinkResource\Pages;
 
 use App\Filament\Resources\LinkResource;
 use Filament\Actions;
+use Filament\Actions\Action;
 use Filament\Resources\Pages\EditRecord;
+use Filament\Support\Enums\ActionSize;
 
 class EditLink extends EditRecord
 {
@@ -13,7 +15,16 @@ class EditLink extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+            Action::make('visit_link')
+            ->outlined()
+            ->label('Visit link')
+            ->icon('heroicon-o-arrow-top-right-on-square')
+            ->size(ActionSize::Small)
+            ->url(fn (): string => config('app.url') . '/' . $this->record->short_id)
+            ->extraAttributes([
+                'target' => '_blank',
+            ]),
+            Actions\DeleteAction::make()
         ];
     }
 }
