9253db5fdd4280e56ab4e64121ba5f685790ae27
Ishan Sheikh <sheikh.ysbh@gmail.com>
2024-10-28 19:01:58 +0530

    added job to delete expired links

---
+++ app/Jobs/DeleteExpiredLinks.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Link;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class DeleteExpiredLinks implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        Link::query()
+            ->where('expires_at', '<', now())
+            ->where('delete_after_expired', 1)
+            ->get()
+            ->each(fn (Link $link) => $link->delete());
+    }
+}
