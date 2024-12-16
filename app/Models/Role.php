bedc59ebea26878298860a7985c06bbc87b1b117
Ishan Sheikh <sheikh.ysbh@gmail.com>
2024-12-16 18:54:50 +0530

    added * for admin permissions

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -27,28 +27,12 @@ class Role extends Model
                 'key' => 'admin',
                 'name' => 'Admin',
                 'description' => 'Admins can perform any action',
-                'permissions' => json_encode([
-                    'team:View',
-                    'team:viewAny',
-                    'team:create',
-                    'team:update',
-                    'team:delete',
-                    'link:view',
-                    'link:viewAny',
-                    'link:create',
-                    'link:update',
-                    'link:delete',
-                    'domain:view',
-                    'domain:viewAny',
-                    'domain:create',
-                    'domain:update',
-                    'domain:delete',
-                ])
+                'permissions' => json_encode([ '*' ])
             ],
             [
                 'key' => 'user',
                 'name' => 'User',
-                'description' => 'User can create, read, update and delete links',
+                'description' => 'User can only manage links',
                 'permissions' => json_encode([
                     'link:viewAny',
                     'link:view',
