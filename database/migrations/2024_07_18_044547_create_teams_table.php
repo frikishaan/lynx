78ebe7d1ef5ac5dcc6fb98cf053e73473bfc2631
Ishan Sheikh <sheikh.ysbh@gmail.com>
2024-12-16 18:55:34 +0530

    removed user_id from teams table

--- database/migrations/2024_07_18_044547_create_teams_table.php
+++ database/migrations/2024_07_18_044547_create_teams_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('teams', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->index();
             $table->string('name');
             $table->tinyInteger('default_team')->default(0);
             $table->timestamps();
